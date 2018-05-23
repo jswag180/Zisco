fab279f6bdb5ad1514aa0002b71a9aab2d139715
BuildTools <unconfigured@null.spigotmc.org>
2018-05-22 23:31:08 -0500

    Small changes.

--- src/main/java/com/zisco/Zisco.java
+++ src/main/java/com/zisco/Zisco.java
@@ -1,9 +1,12 @@
 package com.zisco;
 
+import com.zisco.proxys.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -14,18 +17,30 @@ public class Zisco
     public static final String NAME = "Zisco";
     public static final String VERSION = "1.0";
 
+    @SidedProxy(clientSide = "com.zisco.proxys.ClientProxy", serverSide = "com.zisco.proxys.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static Zisco instance;
+
     private static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        //logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void  postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
     }
 }
