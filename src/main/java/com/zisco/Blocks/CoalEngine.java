fe711a433d8cba01cb082175687f7f5e513af29d
Eugene <eugenetv5@gmail.com>
2018-05-24 22:36:40 -0500

    Added Coal Engine & CoalEngineTileEntity classes

---
+++ src/main/java/com/zisco/Blocks/CoalEngine.java
@@ -0,0 +1,90 @@
+package com.zisco.Blocks;
+
+import com.zisco.Blocks.TileEntitys.CoalEngineTileEntity;
+import com.zisco.Zisco;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+
+public class CoalEngine extends Block implements ITileEntityProvider {
+
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+    public static final PropertyBool ENABLED = PropertyBool.create("enabled");
+
+    public CoalEngine() {
+        super(Material.ROCK);
+
+        setUnlocalizedName(Zisco.MODID + ".coalengine");     // Used for localization (en_US.lang)
+        setRegistryName("coalengine");
+        setCreativeTab(Zisco.CREATIVE_TABS);
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new CoalEngineTileEntity();
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+        worldIn.setBlockState(pos, state.withProperty(ENABLED, true), 3);
+
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        world.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
+    }
+
+    public static EnumFacing getFacingFromEntity(BlockPos clickedBlock, EntityLivingBase entity) {
+        return EnumFacing.getFacingFromVector(
+                (float) (entity.posX - clickedBlock.getX()),
+                (float) (entity.posY - clickedBlock.getY()),
+                (float) (entity.posZ - clickedBlock.getZ()));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState()
+                .withProperty(FACING, EnumFacing.getFront(meta & 7))
+                .withProperty(ENABLED, (meta & 8) != 0);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex() + (state.getValue(ENABLED) ? 8 : 0);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this,FACING,ENABLED);
+    }
+}
---
+++ src/main/java/com/zisco/Blocks/TileEntitys/CoalEngineTileEntitiy.java
@@ -0,0 +1,7 @@
+package com.zisco.Blocks.TileEntitys;
+
+public class CoalEngineTileEntitiy {
+
+
+
+}
